7ed1030cf42a19498a66364105f40bba80f4d391
ValmiDrama <95919292+ValmiDrama@users.noreply.github.com>
2022-06-16 10:00:14 -0300

    Create easy_if_else.dart

---
+++ books/exerciciosDart/easy_if_else.dart
@@ -0,0 +1,19 @@
+void main() {
+    
+    print("Hello Word");
+  
+  bool isFavorito = true;
+  
+  if(isFavorito){print('Favorito');}
+  else{print('Nao Favorito');}
+}
+
+
+class People{
+  String? id;
+  String? name;
+  
+  
+People({this.id, this.name,});
+    
+}
