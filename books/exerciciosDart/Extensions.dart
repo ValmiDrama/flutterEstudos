40dabaa73578a1dcfe80a59b896d90ffad4010a6
ValmiDrama <95919292+ValmiDrama@users.noreply.github.com>
2022-06-21 14:26:05 -0300

    Create Extensions.dart

---
+++ books/exerciciosDart/Extensions.dart
@@ -0,0 +1,40 @@
+void main(){
+  
+  
+  String name = 'biel';
+  print(name);
+  
+  print("\n############### T E S T  1 - Extensions #####################\n");
+ 
+  print(name[0].toUpperCase()+name.substring(1));
+  
+  print(Upcase().toFirstChar(name));
+  
+  
+  
+  
+  print("\n############### T E S T  2 - Extensions #####################\n");
+ 
+  print(name.toFirstChar());
+  
+}
+//extensao nome para a classe STRING
+extension ExtensionsString on String{
+  
+  String toFirstChar(){
+   // return value[0].toUpperCase()+value.substring(1);
+      return this[0].toUpperCase()+ this.substring(1);
+    
+    //this entender que e a variavel passada a 'String'
+    
+  }
+}
+  
+
+class Upcase{
+  
+  toFirstChar(String value){
+    return value[0].toUpperCase()+value.substring(1);
+    
+  }
+}
