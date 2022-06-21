e0785ac8a0b99519c5f5ebdfaafda5ca57152a92
ValmiDrama <95919292+ValmiDrama@users.noreply.github.com>
2022-06-21 10:42:08 -0300

    Create json.dart

---
+++ books/exerciciosDart/json.dart
@@ -0,0 +1,44 @@
+import 'dart:convert';
+
+
+void main(){
+  
+  print("\n############### T E S T  1  #####################\n");
+  
+  String json = '''
+  {
+    "usuario":"Biel",
+    "age":24,
+    "products":[
+          "KY", "BONECA INFLAVEL", "RACAO DOG"
+    ]
+  }
+  ''';
+  
+  print(json);
+  
+  Map resultJsonE = jsonDecode(json);
+  print(resultJsonE["usuario"]);
+  
+ print("\n ############### T E S T  2  #####################\n");
+  
+  Map mapas = {
+    'usuario':'Hallan',
+    'age':69,
+    'products':[
+          'KY', 'MarcaPasso', 'ViagraDiBoi-O-LOL'
+    ]
+    
+    
+  };
+ 
+  var resultJsonD = jsonEncode(mapas);
+  print(resultJsonD);
+  
+  print('\n Produto mais ultilizado pelo \n ${mapas['usuario'].toString()} é:');
+  print("${mapas['products']}");
+  print('O sendo o seu favorito: \n ${mapas['products'][2].toString()}');
+  
+   
+  
+}
